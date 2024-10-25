ebe484aa32d4fad6d77cd4d51339aff0c8c3739b
deepak170601 <deepakreddy1431@gmail.com>
2024-10-25 11:56:59 +0530

    spring-init'

---
+++ src/main/java/com/ust/cicd_app/HelloController.java
@@ -0,0 +1,13 @@
+package com.ust.cicd_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, welcome to your Spring Boot application!";
+    }
+}
